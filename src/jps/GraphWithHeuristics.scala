2cbbf0b884a96067371ff5ce54fab8d398e06305
Martyna Czwarno <m.czwarno.mc@gmail.com>
2018-05-28 23:36:12 +0200

    ida star impl

--- src/jps/GraphWithHeuristics.scala
+++ src/jps/GraphWithHeuristics.scala
@@ -13,13 +13,22 @@ object GraphWithHeuristics {
       Node(3),
       Node(4)
     )
-  val neighborsMap: Map[Option[Node], List[(Option[Node], Int)] ] =
+//  val neighborsMap: Map[Node, List[(Node, Int)] ] =
+//    Map(
+//      nodes(0) -> List((nodes(1), 69), (nodes(3), 135)),
+//      nodes(1) -> List((nodes(0), 69), (nodes(2), 75)),
+//      nodes(2) -> List((nodes(1), 75), (nodes(3), 165)),
+//      nodes(3) -> List((nodes(0), 135), (nodes(2), 165), (nodes(4), 80)),
+//      nodes(4) -> List((nodes(2), 180), (nodes(3), 80))
+//    )
+
+  val neighborsMap: Map[Node, Map[Node, Int] ] =
     Map(
-      Some(nodes(0)) -> List((Some(nodes(1)), 69), (Some(nodes(3)), 135)),
-      Some(nodes(1)) -> List((Some(nodes(0)), 69), (Some(nodes(2)), 75)),
-      Some(nodes(2)) -> List((Some(nodes(1)), 75), (Some(nodes(3)), 165)),
-      Some(nodes(3)) -> List((Some(nodes(0)), 135), (Some(nodes(2)), 165), (Some(nodes(4)), 80)),
-      Some(nodes(4)) -> List((Some(nodes(2)), 180), (Some(nodes(3)), 80))
+      nodes(0) -> Map(nodes(1) -> 69, nodes(3) -> 135),
+      nodes(1) -> Map(nodes(0)-> 69, nodes(2)-> 75),
+      nodes(2) -> Map(nodes(1)-> 75, nodes(3)-> 165),
+      nodes(3) -> Map(nodes(0)-> 135, nodes(2)-> 165, nodes(4)-> 80),
+      nodes(4) -> Map(nodes(2)-> 180, nodes(3)-> 80)
     )
   val heuristics: List[Int] =
     List(
@@ -29,33 +38,41 @@ object GraphWithHeuristics {
       72,
       0
     )
-  val heuristicsMap: Map[Option[Node], Int] = (nodes.map(x=>Option(x)) zip heuristics).toMap
+  val heuristicsMap: Map[Node, Int] = (nodes zip heuristics).toMap
   val firstNode: Option[Node] = nodes.headOption
   val lastNode: Option[Node] = nodes.lastOption
-  val startLimit: Option[Int] = heuristicsMap.get(firstNode)
+  val startLimit: Option[Int] = heuristicsMap.get(firstNode.get)
 
   //returns path from start to the end and total cost
   def idaStar(): (List[Node], Int) = {
+    if (firstNode.isEmpty)
+      throw new Exception("The graph is empty")
     deepen(startLimit)
   }
 
   def deepen(limit: Option[Int]): (List[Node], Int) = {
-    iteration(List(nodes(0)), 0, limit)
+    iteration(firstNode, List(), 0, limit)
+    //iteration(None, List(), 0, limit)
   }
 
-  def iteration(nodes: List[Node], cost: Int, limit: Option[Int]): (List[Node], Int) = {
-    val first: Option[Node] = nodes.headOption
+  def iteration(first: Option[Node], nodes: List[Node], cost: Int, limit: Option[Int]): (List[Node], Int) = {
+    //val first: Option[Node] = nodes.headOption
     first match {
       case `lastNode` => (List(first.get), cost)
-      case None => (List(), 0)
+      //case None => (List(), 0)
       case _ => {
-        val neighbors = neighborsMap.getOrElse(first, List()).unzip._1 //lista samych sasiadow
-        println(neighbors)
-        //println(nodes.tail)
-        val newNodes: List[Node] = flatten(neighbors :: nodes.tail)
-        println(newNodes)
-        val first2 = newNodes.headOption
-        val newCost = cost + neighborsMap.get(first).get()
+
+          val neighbors: List[Node] = neighborsMap.getOrElse(first.get, Map()).keySet.toList //lista samych sasiadow
+          //println(neighbors)
+          //println(nodes.tail)
+          val newNodes: List[Node] = neighbors.filter(x => !nodes.exists(y => y == x)) ::: nodes
+          //println(newNodes)
+          val head: Option[Node] = newNodes.headOption
+
+          val updatedCost: Int = cost + neighborsMap.getOrElse(first.get, Map()).getOrElse(head.get,0)
+          println(updatedCost)
+
+
 
 
         //temporary
