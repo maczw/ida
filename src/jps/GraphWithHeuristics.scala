7eb4bacd332384d47949ccc2091c6c34c1f78797
Martyna Czwarno <m.czwarno.mc@gmail.com>
2018-05-31 22:50:47 +0200

    another example of graph

--- src/jps/GraphWithHeuristics.scala
+++ src/jps/GraphWithHeuristics.scala
@@ -21,6 +21,7 @@ object GraphWithHeuristics {
       nodes(3) -> Map(nodes(0)-> 135, nodes(2)-> 165, nodes(4)-> 80),
       nodes(4) -> Map(nodes(2)-> 180, nodes(3)-> 80)
     )
+
   val heuristics: List[Int] =
     List(
       200,
@@ -33,6 +34,47 @@ object GraphWithHeuristics {
   val firstNode: Option[Node] = nodes.headOption
   val lastNode: Option[Node] = nodes.lastOption
   val startLimit: Option[Int] = heuristicsMap.get(firstNode.get)
+
+//  val nodes: Vector[Node] =
+//    Vector(
+//      Node(0),
+//      Node(1),
+//      Node(2),
+//      Node(3),
+//      Node(4),
+//      Node(5),
+//      Node(6),
+//      Node(7),
+//    )
+//
+//  val neighborsMap: Map[Node, Map[Node, Int] ] =
+//    Map(
+//      nodes(0) -> Map(nodes(1) -> 6, nodes(2) -> 5, nodes(3) -> 10),
+//      nodes(1) -> Map(nodes(0)-> 6, nodes(4)-> 6),
+//      nodes(2) -> Map(nodes(0)-> 5, nodes(4)-> 6, nodes(5)-> 7),
+//      nodes(3) -> Map(nodes(0)-> 10, nodes(5)-> 6),
+//      nodes(4) -> Map(nodes(1)-> 6, nodes(2)-> 6, nodes(6)-> 4),
+//      nodes(5) -> Map(nodes(2)-> 7, nodes(3)-> 6, nodes(6)-> 6),
+//      nodes(6) -> Map(nodes(4)-> 4, nodes(5)-> 6, nodes(7)-> 3),
+//      nodes(7) -> Map(nodes(6)-> 3)
+//    )
+//
+//  val heuristics: List[Int] =
+//    List(
+//      12,
+//      10,
+//      13,
+//      4,
+//      4,
+//      2,
+//      1,
+//      0
+//    )
+//
+//  val heuristicsMap: Map[Node, Int] = (nodes zip heuristics).toMap
+//  val firstNode: Option[Node] = nodes.headOption
+//  val lastNode: Option[Node] = nodes.lastOption
+//  val startLimit: Option[Int] = heuristicsMap.get(firstNode.get)
 
   //returns path from start to the end and total cost
   def idaStar(): (List[Node], Int) = {
