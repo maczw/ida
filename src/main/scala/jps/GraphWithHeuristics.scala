f78bb7f4fc7ace356fd733c53903d49f84f9d496
unknown <milena.jachimowska@gmail.com>
2018-06-02 14:18:30 +0200

    Api Doc

--- src/main/scala/jps/GraphWithHeuristics.scala
+++ src/main/scala/jps/GraphWithHeuristics.scala
@@ -3,39 +3,92 @@ package scala.jps
 import scala.annotation.tailrec
 import scala.collection.immutable.{List, Map, SortedSet, Vector}
 import scala.io.Source
-
+/**
+  * A class to represent the graph and implement IDA* algoritm [[https://en.wikipedia.org/wiki/Iterative_deepening_A*]]
+  *
+  * Specify `path` to your resources when creating a new `GraphWithHeuristics`
+  * your resources should contain:
+  *
+  * @author Martyna Czwarno, Milena Jachimowska
+  * @version 1.0
+  * @see [[https://github.com/maczw/ida]] - our repository on github
+  *
+  * @example
+  *
+  * run scala.jps.Main "C:\ida\src\main\resources\graph1"
+  *
+  *
+  * ==Your resources should contain three files defining graph:==
+  * nodes: {{{
+  *   1
+  *   2
+  *   3
+  *   4
+  *   5
+  * }}}
+  *
+  * heuristics: {{{
+  *   200
+  *   247
+  *   162
+  *   72
+  *   0
+  * }}}
+  *
+  * neighbors: {{{
+  *   0->1-69,3-135
+  *   1->0-69,2-75
+  *   2->1-75,3-165
+  *   3->0-135,2-165,4-80
+  *   4->2-180,3-80
+  * }}}
+  *
+  *
+  *
+  */
 class GraphWithHeuristics(val path: String) {
+
+  /**
+    * A class to represent a single node in the graph
+    * @param key label of the node
+    */
   case class Node(key: Int)
 
-//  val nodes: Vector[Node] =
-//    Vector(
-//      Node(0),
-//      Node(1),
-//      Node(2),
-//      Node(3),
-//      Node(4)
-//    )
+  //  val nodes: Vector[Node] =
+  //    Vector(
+  //      Node(0),
+  //      Node(1),
+  //      Node(2),
+  //      Node(3),
+  //      Node(4)
+  //    )
 
-//  val neighborsMap: Map[Node, Map[Node, Int] ] =
-//    Map(
-//      nodes(0) -> Map(nodes(1) -> 69, nodes(3) -> 135),
-//      nodes(1) -> Map(nodes(0)-> 69, nodes(2)-> 75),
-//      nodes(2) -> Map(nodes(1)-> 75, nodes(3)-> 165),
-//      nodes(3) -> Map(nodes(0)-> 135, nodes(2)-> 165, nodes(4)-> 80),
-//      nodes(4) -> Map(nodes(2)-> 180, nodes(3)-> 80)
-//    )
+  //  val neighborsMap: Map[Node, Map[Node, Int] ] =
+  //    Map(
+  //      nodes(0) -> Map(nodes(1) -> 69, nodes(3) -> 135),
+  //      nodes(1) -> Map(nodes(0)-> 69, nodes(2)-> 75),
+  //      nodes(2) -> Map(nodes(1)-> 75, nodes(3)-> 165),
+  //      nodes(3) -> Map(nodes(0)-> 135, nodes(2)-> 165, nodes(4)-> 80),
+  //      nodes(4) -> Map(nodes(2)-> 180, nodes(3)-> 80)
+  //    )
 
-//  val heuristics: List[Int] =
-//    List(
-//      200,
-//      247,
-//      162,
-//      72,
-//      0
-//    )
+  //  val heuristics: List[Int] =
+  //    List(
+  //      200,
+  //      247,
+  //      162,
+  //      72,
+  //      0
+  //    )
 
+  /**
+    * vector of nodes read from file
+    */
   val nodes: Vector[Node] = Source.fromFile(path + "//" + "nodes").getLines.map(x=>Node(x.toInt)).toVector
 
+  /**
+    * map connecting nodes to their neighbors
+    */
   val neighborsMap: Map[Node, Map[Node, Int] ] = {
     Source.fromFile(path + "//" + "neighbors").getLines
       .map(m =>
@@ -53,18 +106,36 @@ class GraphWithHeuristics(val path: String) {
       .toMap
   }
 
+  /**
+    * list of heuristic function values read from file
+    */
   val heuristics: List[Int] = Source.fromFile(path + "//" + "heuristics").getLines.map(_.toInt).toList
 
-//  println(nodes)
-//  println(neighborsMap)
-//  println(heuristics)
-
+  //  println(nodes)
+  //  println(neighborsMap)
+  //  println(heuristics)
+  /**
+    * map connecting nodes with their values of heuristic function
+    */
   val heuristicsMap: Map[Node, Int] = (nodes zip heuristics).toMap
+  /**
+    *  the first node of the path
+    */
   val firstNode: Option[Node] = nodes.headOption
+  /**
+    *  the final node of the path
+    */
   val lastNode: Option[Node] = nodes.lastOption
+  /**
+    * limit for the first recursion of IDA* algorithm is the value of heuristic function of the starting node
+    */
   val startLimit: Option[Int] = heuristicsMap.get(firstNode.get)
 
-  //returns path from start to the end and total cost
+  /**
+    * implementation of IDA* algorithm
+    * @return path from the first to the last node and total cost
+    * @throws Exception if given graph is empty
+    */
   def idaStar(): (List[Node], Int) = {
     if (firstNode.isEmpty)
       throw new Exception("The graph is empty")
@@ -72,6 +143,11 @@ class GraphWithHeuristics(val path: String) {
     (result._1, result._2)
   }
 
+  /**
+    * one recursion of the algorithm
+    * @param limit
+    * @return current path, limit,
+    */
   @tailrec
   private def deepen(limit: Option[Int]): (List[Node], Int, SortedSet[Int]) = {
     val result = iteration(firstNode, List(), 0, limit,true)
@@ -81,8 +157,22 @@ class GraphWithHeuristics(val path: String) {
       result
   }
 
+  /**
+    * increments limit for IDA* algorithm
+    * @param availableLimits
+    * @return incremented limit - the next minimal heuristic function value of the neighbors
+    */
   def incrementLimit(availableLimits: SortedSet[Int] ): Option[Int] = Option(availableLimits.min)
 
+  /**
+    * one recursion of IDA* algorithm
+    * @param first first node of the searched path
+    * @param nodes last node of the searched path
+    * @param cost cost of the current path
+    * @param limit IDA* limit for the current recursion
+    * @param flag true if the recursion starts with a new limit
+    * @return current path,
+    */
   def iteration(first: Option[Node], nodes: List[Node], cost: Int, limit: Option[Int], flag: Boolean): (List[Node], Int, SortedSet[Int]) = {
     first match {
       case `lastNode` => (List(first.get), cost, SortedSet())
--- src/main/scala/jps/Main.scala
+++ src/main/scala/jps/Main.scala
@@ -1,6 +1,8 @@
 package scala.jps
 
-
+/**
+  * Starting point of the application
+  */
 object Main extends App{
   override def main(args: Array[String]) {
     println("Path and total cost: ", new GraphWithHeuristics(args(0)).idaStar())
